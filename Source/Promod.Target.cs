d3bce06c4530147aa81599e143cd7bdda7d67c28
Narknon <narknon0@gmail.com>
2022-12-13 13:31:15 -0500

    tweaks

---
+++ Source/Promod.Target.cs
@@ -0,0 +1,11 @@
+﻿using UnrealBuildTool;
+
+public class PromodTarget : TargetRules {
+	public PromodTarget(TargetInfo Target) : base(Target) {
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange(new string[] {
+			"Promod",
+		});
+	}
+}
--- Source/Promod/Private/PromodModule.cpp
+++ Source/Promod/Private/PromodModule.cpp
@@ -1,3 +1,3 @@
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_MODULE(FDefaultGameModuleImpl, Promod);
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, Promod, "Promod");
