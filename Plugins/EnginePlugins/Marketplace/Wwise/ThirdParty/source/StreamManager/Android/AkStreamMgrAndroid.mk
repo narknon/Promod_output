722f1e517c6f67f20735f0b4f371d92a525fb1e4
Narknon <narknon0@gmail.com>
2022-12-13 10:49:02 -0500

    Fix wwise integration

--- Plugins/EnginePlugins/Marketplace/Wwise/ThirdParty/source/StreamManager/Android/AkStreamMgrAndroid.mk
+++
@@ -1,3 +0,0 @@
-workspace_AKSTREAMMGRANDROID_PATH := $(call my-dir)
-
-include $(workspace_AKSTREAMMGRANDROID_PATH)/AkStreamMgr.mk
---
+++ Source/Promod_Dump.Target.cs
@@ -0,0 +1,17 @@
+﻿using UnrealBuildTool;
+
+public class Promod_DumpTarget : TargetRules {
+	public Promod_DumpTarget(TargetInfo Target) : base(Target) {
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange(new string[] {
+			"GameEventViz",
+			"MinimapPlugin",
+			"NuclearCore",
+			"OnlineSubsystemSteam",
+			"PlayFabSubsystem",
+			"Promod",
+			"SMExtendedRuntime",
+		});
+	}
+}
